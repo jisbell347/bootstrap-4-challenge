c51ce84482e18d09c82140374a3fb93200f77a76
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-01 13:08:11 -0600

    Changed jumbotron text

--- index.php
+++ index.php
@@ -32,10 +32,10 @@
 		<main>
 			<div class="container">
 				<div class="jumbotron">
-					<h1 class="display-4">Hello, world!</h1>
-					<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+					<h1 class="display-4">Pandas!!</h1>
+					<p class="lead">Talk about random stuff! I mean, Wow!</p>
 					<hr class="my-4">
-					<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+					<p>Space for sale. Contact us at 555-0938 for more info about taco-eating kittens.</p>
 					<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 				</div>
 			</div>
